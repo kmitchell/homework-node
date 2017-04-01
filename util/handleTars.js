fadaccbaa324fab94037d98b98ca8b2fc4aafeba
Kevin Mitchell <giudecca@gmail.com>
2017-03-31 18:51:05 -0700

    Promisify request in handleTars
    Previous code wasn't properly returning requests as promises, partially breaking the chain in index. Also, some minor readability changes.

--- util/handleTars.js
+++ util/handleTars.js
@@ -6,15 +6,20 @@ const Promise = require('bluebird')
 // Asynchronously downloads and extracts package tars into
 // appropriate folders, via a list from tarballUrls.js
 var handleTars = function (packageMetadatas) {
-  return Promise.map(packageMetadatas, (function(promise) {
-    var name = promise[0], url = promise[1]
-      return request
-        .get(url)
-        .on('error', (error) => console.log(error))
+  return Promise.map(packageMetadatas, makeRequest)
+
+  function makeRequest(metadata) {
+    return new Promise (function (resolve, reject) {
+      request
+        .get(metadata[1])
+        .on('error', (error) => { return reject ((error) => console.log(error))})
         .pipe(gunzip())
-        .pipe(tar.extract(`./packages/${name}`))
-        .on('finish', () => console.log(`* Downloaded and extracted '${name}' successfully!`))
-  }))
+        .pipe(tar.extract(`./packages/${metadata[0]}`))
+        .on('finish', function () {
+          return resolve (console.log(`* Downloaded and extracted '${metadata[0]}'' successfully!`))
+        })
+    })
+  }
 }
 
 module.exports = handleTars
