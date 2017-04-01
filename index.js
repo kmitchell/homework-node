cb59c5a8eebf8d4b30878fb1d07b9087b6d22647
Kevin Mitchell <giudecca@gmail.com>
2017-03-31 18:32:15 -0700

    Fix the promise chain to work with test suite
    The previous chain wasn't waiting for the async requests to finish. This change both fixes the app to work with the test suite, and runs the promise chain as intended.

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const COUNT = parseInt(process.env.COUNT, 10) || 10
 
 module.exports = downloadPackages
 
-function downloadPackages (count) {
+function downloadPackages (count, callback) {
   console.log(`Beginning download of top ${COUNT} packages...`)
 
   getTopPackagesMetadata(count)
     .then(getTarballUrls)
     .then(handleTars)
+    .then(() => callback() )
+    .then(() => console.log("All done."))
     .catch((error) => console.log(error))
-}
 
-downloadPackages(COUNT)
+}
